ba4381a4551b0354e2add4751f9e74c195a2796f
Mason Wudtee <mason.wudtee@gmail.com>
2022-08-24 15:16:59 -0500

    exercise 8 more or less complete

--- code.js
+++ code.js
@@ -5,6 +5,17 @@
 
 // sayHello function
     function sayHello(input) {
+        if (input === undefined || input === true || input === false) {
+            return "Hello, World!";
+        } else if (input === input) {
+    return "Hello, " + input + "!";
+}
+        // if (input === true) {
+        //     return "Hello, World!";
+        // } else if (input === false) {
+        //     return "Hello, World!";
+        //
+
         // if (input === "Alex"){
         //     return "Hello Alex!";
         // } else if (input === "Jane"){
@@ -12,10 +23,17 @@
         // } else if (input === "Pat") {
         //     return "Hello Pat!";
         // }
-        if (input) {
-            return "Hello " + input + "!";
-        }
-        else {
-            return "Hello World!"
-        }
+
+        // if (input) {
+        //     return "Hello " + input + "!";
+        // }
+        // else if (input === true) {
+        //     return "Hello, World!";
+        // } else if (input === false) {
+        //     return "Hello false!";
+        // }
+        // else {
+        //     return "Hello, World!"
+        // }
     }
+
--- tests.js
+++ tests.js
@@ -23,19 +23,24 @@ describe('sayHello', function() {
         expect(typeof sayHello()).toBe("string");
     });
     it('should return the string "Hello World! when executed', function() {
-        expect(sayHello()).toBe(`Hello World!`);
+        expect(sayHello()).toBe("Hello, World!");
     });
     it('should return the string "Hello Jane! when executed', function() {
-        expect(sayHello("Jane")).toBe(`Hello Jane!`);
+        expect(sayHello("Jane")).toBe(`Hello, Jane!`);
     });
     it('should return the string "Hello Pat! when executed', function() {
-        expect(sayHello("Pat")).toBe(`Hello Pat!`);
+        expect(sayHello("Pat")).toBe(`Hello, Pat!`);
     });
     it('should return the string "Hello Alex! when executed', function() {
-        expect(sayHello("Alex")).toBe(`Hello Alex!`);
+        expect(sayHello("Alex")).toBe(`Hello, Alex!`);
     });
     it("should never return 'undefined' when called", function() {
-        expect(sayHello()).not.toBe(undefined);
+        expect(sayHello()).toBe("Hello, World!");
     });
-
+    it('should return the string "Hello, World!" when true', function() {
+        expect(sayHello(true)).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World!" when executed', function() {
+        expect(sayHello(false)).toBe("Hello, World!");
+    });
 });
