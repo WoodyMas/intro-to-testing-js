c29ef4e000932e1a96836e1677a24e8d3f80459b
Mason Wudtee <mason.wudtee@gmail.com>
2022-08-24 12:14:48 -0500

    Exercise 7 completion

--- code.js
+++ code.js
@@ -5,12 +5,15 @@
 
 // sayHello function
     function sayHello(input) {
-        if (input === "Alex"){
-            return "Hello Alex!";
-        } else if (input === "Jane"){
-            return "Hello Jane!";
-        } else if (input === "Pat") {
-            return "Hello Pat!";
+        // if (input === "Alex"){
+        //     return "Hello Alex!";
+        // } else if (input === "Jane"){
+        //     return "Hello Jane!";
+        // } else if (input === "Pat") {
+        //     return "Hello Pat!";
+        // }
+        if (input) {
+            return "Hello " + input + "!";
         }
         else {
             return "Hello World!"
